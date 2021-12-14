69358292a65987a52d34fd5881a6c2498c36ed2f
your name <xuanthanh.110699@gmail.com>
2021-12-14 16:45:38 +0700

    up

--- assets/js/slick.js
+++ assets/js/slick.js
@@ -5,9 +5,23 @@ $('.slick-slider').slick({
     nextArrow: $('.next'),
     infinite: true,
     autoplay: true,
-    autoplaySpeed: 3000,
+    autoplaySpeed: 4000,
     slidesToShow: 3,
-    slidesToScroll: 1,
+    slidesToScroll: 2,
+    variableWidth: false,
+    speed: 2500
+});
+
+$('.partner-slider').slick({
+    dots: false,
+    arrows: true,
+    prevArrow: $('.pre'),
+    nextArrow: $('.next'),
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    slidesToShow: 6,
+    slidesToScroll: 3,
     variableWidth: false,
     speed: 2500
 });
