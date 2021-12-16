7eb91c0e5b777a776679c89b169df4d09adeea04
your name <xuanthanh.110699@gmail.com>
2021-12-16 11:35:44 +0700

    fix

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,31 @@
 const header = document.querySelector('.header');
 const menuitems = document.querySelectorAll('.item-link');
 const topbutton = document.querySelector(".scrolltop");
+const openmenu = document.querySelector('.mobile-open');
+const closenmenu = document.querySelector('.mobile-close');
+const menumodal = document.querySelector('.header-nav');
+const mobilemenu = document.querySelector('.navbar');
+
+
 window.onscroll = function() {scrollFunction()};
+
+function openMenu() {
+    openmenu.style.display = 'none';
+    closenmenu.style.display = 'block';
+    menumodal.classList.add('menu-active');
+    mobilemenu.classList.add('menufly');
+}
+
+function closeMenu() {
+    openmenu.style.display = 'block';
+    closenmenu.style.display = 'none';
+    mobilemenu.classList.remove('menufly');
+    menumodal.classList.remove('menu-active');
+}
+
+openmenu.addEventListener('click', openMenu);
+closenmenu.addEventListener('click', closeMenu);
+
 
 function scrollFunction () {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
