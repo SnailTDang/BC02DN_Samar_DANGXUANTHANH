b427784f57aba355b086332123f5e5368f1cd8d8
ThanhDang <xuanthanh.110699@gmail.com>
2021-12-26 03:38:45 +0700

    fix

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,61 +1,62 @@
-const header = document.querySelector('.header');
-const menuitems = document.querySelectorAll('.item-link');
+const header = document.querySelector(".header");
+const menuitems = document.querySelectorAll(".item-link");
 const topbutton = document.querySelector(".scrolltop");
-const openmenu = document.querySelector('.mobile-open');
-const closenmenu = document.querySelector('.mobile-close');
-const menumodal = document.querySelector('.header-nav');
-const mobilemenu = document.querySelector('.navbar');
-const mobileItems = document.querySelectorAll('.menu-item');
+const openmenu = document.querySelector(".mobile-open");
+const closenmenu = document.querySelector(".mobile-close");
+const menumodal = document.querySelector(".header-nav");
+const mobilemenu = document.querySelector(".navbar");
+const mobileItems = document.querySelectorAll(".menu-item");
 
-mobileItems.forEach(item => {
-    item.addEventListener('click', function(e){
+mobileItems.forEach((item) => {
+  let itemMobile = item.querySelector("a");
+  item.addEventListener("click", function (e) {
+    this.querySelector(".sub-menu").classList.toggle("sub-menu-open");
+    itemMobile.querySelector("i").classList.toggle("fa-angle-right");
+    itemMobile.querySelector("i").classList.toggle("fa-angle-down");
+  });
+  itemMobile.addEventListener("click", function (e) {
+    console.log(e);
+    e.preventDefault();
+  });
+});
 
-        this.querySelector('.sub-menu').classList.toggle('sub-menu-open');
-        this.querySelector('a').querySelector('i').classList.toggle('fa-angle-right');
-        this.querySelector('a').querySelector('i').classList.toggle('fa-angle-down');
-    })
-})
-
-
-window.addEventListener('scroll', function() {
-    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-        header.classList.add('whitebg');
-        topbutton.style.display = "block";
-        for (const menuitem of menuitems) {
-        menuitem.classList.add('item-link-scroll');
-        }
-        
-    }
-    else {
-        header.classList.remove('whitebg');
-        topbutton.style.display = "none";
-        for (const menuitem of menuitems) {
-            menuitem.classList.remove('item-link-scroll');
-        }
-    }
+mobilemenu.addEventListener("click", function (e) {
+  e.stopPropagation();
 });
 
 function openMenu() {
-    openmenu.style.display = 'none';
-    closenmenu.style.display = 'block';
-    menumodal.classList.toggle('menu-active');
-    mobilemenu.classList.toggle('menufly');
+  openmenu.style.display = "none";
+  closenmenu.style.display = "block";
+  menumodal.classList.toggle("menu-active");
+  mobilemenu.classList.toggle("menufly");
 }
 
 function closeMenu() {
-    openmenu.style.display = 'block';
-    closenmenu.style.display = 'none';
-    mobilemenu.classList.toggle('menufly');
-    menumodal.classList.toggle('menu-active');
+  openmenu.style.display = "block";
+  closenmenu.style.display = "none";
+  mobilemenu.classList.toggle("menufly");
+  menumodal.classList.toggle("menu-active");
 }
 
-mobilemenu.addEventListener('click', function(e) {
-    e.stopPropagation();
-})
+window.addEventListener("scroll", function () {
+  if (
+    document.body.scrollTop > 100 ||
+    document.documentElement.scrollTop > 100
+  ) {
+    header.classList.add("whitebg");
+    topbutton.style.display = "block";
+    for (const menuitem of menuitems) {
+      menuitem.classList.add("item-link-scroll");
+    }
+  } else {
+    header.classList.remove("whitebg");
+    topbutton.style.display = "none";
+    for (const menuitem of menuitems) {
+      menuitem.classList.remove("item-link-scroll");
+    }
+  }
+});
 
-openmenu.addEventListener('click', openMenu);
-closenmenu.addEventListener('click', closeMenu);
-menumodal.addEventListener('click',closeMenu);
-
-
-
+openmenu.addEventListener("click", openMenu);
+closenmenu.addEventListener("click", closeMenu);
+menumodal.addEventListener("click", closeMenu);
