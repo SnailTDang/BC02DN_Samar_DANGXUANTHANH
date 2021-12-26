32bc4c272f5a6364dc64f43b8f75963c71d5ecde
ThanhDang <xuanthanh.110699@gmail.com>
2021-12-26 18:26:42 +0700

    fix

--- assets/js/header.js
+++ assets/js/header.js
@@ -6,18 +6,21 @@ const closenmenu = document.querySelector(".mobile-close");
 const menumodal = document.querySelector(".header-nav");
 const mobilemenu = document.querySelector(".navbar");
 const mobileItems = document.querySelectorAll(".menu-item");
+const screenWidth = document.body.clientWidth;
 
 mobileItems.forEach((item) => {
   let itemMobile = item.querySelector("a");
-  item.addEventListener("click", function (e) {
-    this.querySelector(".sub-menu").classList.toggle("sub-menu-open");
-    itemMobile.querySelector("i").classList.toggle("fa-angle-right");
-    itemMobile.querySelector("i").classList.toggle("fa-angle-down");
-  });
-  itemMobile.addEventListener("click", function (e) {
-    console.log(e);
-    e.preventDefault();
-  });
+  if (screenWidth < 992) {
+    console.log(screenWidth);
+    itemMobile.addEventListener("click", function (e) {
+      e.preventDefault();
+    });
+    item.addEventListener("click", function (e) {
+      this.querySelector(".sub-menu").classList.toggle("sub-menu-open");
+      itemMobile.querySelector("i").classList.toggle("fa-angle-right");
+      itemMobile.querySelector("i").classList.toggle("fa-angle-down");
+    });
+  }
 });
 
 mobilemenu.addEventListener("click", function (e) {
